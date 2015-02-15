142ef222a491a269a5733b99c4925c1fe37dd87a
Clifford Anderson <anderson.clifford@gmail.com>
2015-02-14 20:52:05 -0600

    refinements to bounding box

--- scripts/map.js
+++ scripts/map.js
@@ -2,53 +2,53 @@
 
 // Construct a bounding box for this map that the user cannot
 // move out of
-var southWest = L.latLng(52.493, 13.458),
-  northEast = L.latLng(52.5453, 13.328),
-  bounds = L.latLngBounds(southWest, northEast);
+var southWest = L.latLng(52.500, 13.450),
+	northEast = L.latLng(52.545, 13.328),
+	bounds = L.latLngBounds(southWest, northEast);
 
 var map = L.mapbox.map('map', 'vulibrarygis.l36anlai', {
-  maxBounds: bounds,
-  minZoom: 13,
-  maxZoom: 16
+	maxBounds: bounds,
+	minZoom: 13,
+	maxZoom: 16
 });
 
 // Add custom popup html to each marker
 map.markerLayer.on('layeradd', function(e) {
-  var marker = e.layer;
-  var feature = marker.feature;
-  var images = feature.properties.images
-  var slideshowContent = '';
+	var marker = e.layer;
+	var feature = marker.feature;
+	var images = feature.properties.images
+	var slideshowContent = '';
 
-  for (var i = 0; i < images.length; i++) {
-    var img = images[i];
+	for (var i = 0; i < images.length; i++) {
+		var img = images[i];
 
-    slideshowContent += '<div class="image' + (i === 0 ? ' active' : '') +
-      '">' +
-      img[0] +
-      '<div class="caption">' + img[1] + '</div>' +
-      '</div>';
-  }
+		slideshowContent += '<div class="image' + (i === 0 ? ' active' : '') +
+			'">' +
+			img[0] +
+			'<div class="caption">' + img[1] + '</div>' +
+			'</div>';
+	}
 
-  // Create custom popup content
-  var popupContent = '<div id="' + feature.properties.id +
-    '" class="popup">' +
-    '<h2>' + feature.properties.title + '</h2>' +
-    '<div class="slideshow">' +
-    slideshowContent +
-    '</div>' +
-    '<div class="cycle">' +
-    '<a href="#" class="prev">&laquo; Previous</a>' +
-    '<a href="#" class="next">Next &raquo;</a>' +
-    '</div>'
-  '</div>';
+	// Create custom popup content
+	var popupContent = '<div id="' + feature.properties.id +
+		'" class="popup">' +
+		'<h2>' + feature.properties.title + '</h2>' +
+		'<div class="slideshow">' +
+		slideshowContent +
+		'</div>' +
+		'<div class="cycle">' +
+		'<a href="#" class="prev">&laquo; Previous</a>' +
+		'<a href="#" class="next">Next &raquo;</a>' +
+		'</div>'
+	'</div>';
 
-  // http://leafletjs.com/reference.html#popup
-  marker.bindPopup(popupContent, {
-    closeButton: false,
-    maxWidth: 200,
-    autoPan: true,
-    keepInView: true
-  });
+	// http://leafletjs.com/reference.html#popup
+	marker.bindPopup(popupContent, {
+		closeButton: false,
+		maxWidth: 200,
+		autoPan: true,
+		keepInView: true
+	});
 });
 
 // Add features to the map
@@ -57,24 +57,24 @@ map.markerLayer.setGeoJSON(geoJson);
 // This example uses jQuery to make selecting items in the slideshow easier.
 // Download it from http://jquery.com
 $('#map').on('click', '.popup .cycle a', function() {
-  var $slideshow = $('.slideshow'),
-    $newSlide;
+	var $slideshow = $('.slideshow'),
+		$newSlide;
 
-  if ($(this).hasClass('prev')) {
-    $newSlide = $slideshow.find('.active').prev();
-    if ($newSlide.index() < 0) {
-      $newSlide = $('.image').last();
-    }
-  } else {
-    $newSlide = $slideshow.find('.active').next();
-    if ($newSlide.index() < 0) {
-      $newSlide = $('.image').first();
-    }
-  }
+	if ($(this).hasClass('prev')) {
+		$newSlide = $slideshow.find('.active').prev();
+		if ($newSlide.index() < 0) {
+			$newSlide = $('.image').last();
+		}
+	} else {
+		$newSlide = $slideshow.find('.active').next();
+		if ($newSlide.index() < 0) {
+			$newSlide = $('.image').first();
+		}
+	}
 
-  $slideshow.find('.active').removeClass('active').hide();
-  $newSlide.addClass('active').show();
-  return false;
+	$slideshow.find('.active').removeClass('active').hide();
+	$newSlide.addClass('active').show();
+	return false;
 });
 
-map.setView([52.51959, 13.40567], 13);
+map.setView([52.545, 13.328], 13);
